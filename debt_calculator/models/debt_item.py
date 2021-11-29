4749782aafc84e375d10253360f2d1c2155dc2ff
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2021-11-29 09:12:40 +0100

    Add additional fields that need to be decorated

--- debt_calculator/models/debt_item.py
+++ debt_calculator/models/debt_item.py
@@ -1,11 +1,13 @@
-from dataclasses import dataclass
 from datetime import datetime
 
+from pydantic import BaseModel
 
-@dataclass
-class DebtItem:
+
+class DebtItem(BaseModel):
     name: str
     total_amount: float
     number_of_installments: int
     installment_amount: float
     start_date: datetime
+    remaining_installments: int = None
+    remaining_amount: float = None
